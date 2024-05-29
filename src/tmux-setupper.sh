aceeab60ded332b701fa93de1c2a5fdc91eb0b9f
Alessandro Paganelli <alessandro.paganelli@zuru.com>
2024-05-29 23:29:24 +0200

    Update to tmux-setupper to include knowledege wiki

--- src/tmux-setupper.sh
+++ src/tmux-setupper.sh
@@ -8,9 +8,14 @@ tmux has-session -t $SESSION_NAME
 if [[ $? != 0 ]]; then
     tmux new -s $SESSION_NAME -n vim -d
     tmux send-keys -t $SESSION_NAME "cd $WORKSPACE" C-m
+    tmux send-keys -t $SESSION_NAME "uact" C-m
     tmux send-keys -t $SESSION_NAME "vim" C-m
     tmux new-window -n terminal -t $SESSION_NAME
     tmux send-keys -t $SESSION_NAME:1 "cd $WORKSPACE" C-m
+    tmux send-keys -t $SESSION_NAME:1 "uact" C-m
+    tmux new-window -n knowledge -t $SESSION_NAME
+    tmux send-keys -t $SESSION_NAME:2 "cd ~/workspace/knowledge_wiki" C-m
+    tmux send-keys -t $SESSION_NAME:2 "vim" C-m
     tmux select-window -t $SESSION_NAME:0
 fi
 
